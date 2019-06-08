4e04484e9293a021d51b5ff95465bd861be8885f
Madhuris12 <49709761+Madhuris12@users.noreply.github.com>
2019-06-08 17:45:48 +0530

    Update and rename TrackServiceImpl.java to MusixServiceImpl.java

--- src/main/java/com/stackroute/musixservice/service/MusixServiceImpl.java
+++ src/main/java/com/stackroute/musixservice/service/MusixServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
     @Service
-    public class TrackServiceImpl implements TrackService {
+    public class MusixServiceImpl implements TrackService {
 
         @Autowired
         private TrackRepository trackRepository;
